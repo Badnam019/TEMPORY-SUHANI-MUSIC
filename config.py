cc577d24e88572f7631334707e909c6f1f19192a
BADNAM XD <yadavxd746@gmail.com>
2025-04-04 22:17:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/venompower/TEMPORY-SUHANI-MUSIC",
+    "https://github.com/Badnam019/TEMPORY-SUHANI-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
